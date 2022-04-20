b428c17d8a11a29d40cdd19ebf0118643c67f23e
Gabriel Vieira dos Santos <gabrieldepiata@outlook.com>
2022-04-19 23:24:46 -0300

    criar-strings-arrays

---
+++ modulo-1/strings-arrays/template_javascript/index.js
@@ -0,0 +1,62 @@
+/*  Exercícios de interpretação de código
+
+    EXERCÍCIO 1 
+
+let array
+console.log('a. ', array) a.INDEFINIDO
+
+array = null
+console.log('b. ', array) b.NULL
+
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('c. ', array.length) c. 11 
+
+let i = 0
+console.log('d. ', array[i]) d. 3
+
+array[i+1] = 19
+console.log('e. ', array) c. 3,19,5,6,7,8,9,10,11,12,13
+
+const valor = array[i+6]
+console.log('f. ', valor) f. 9 
+
+    EXERCÍCIO 2 
+
+const frase = prompt("Digite uma frase")
+console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length) SUBI NUM ÔNIBUS EM MIRROCOS
+
+ ---------   Exercícios de escrita de código
+
+    EXERCÍCIO 1 
+
+let email = prompt ("Digite seu e-mail")
+let nome = prompt ("Digite seu nome")
+console.log(`O e-mail `  +email+  ` foi cadastrado com sucesso. Seja bem vindo `+nome+`!`)
+
+    EXERCÍCIO 2 
+
+let comidasFavoritas = ["Feijoada", "Pizza", "Macarrão", "Sanduíche", "Cuscuz"]
+console.log(comidasFavoritas);
+console.log("Essas são as minhas comidas preferidas:")
+console.log(comidasFavoritas[0])
+console.log(comidasFavoritas[1])
+console.log(comidasFavoritas[2])
+console.log(comidasFavoritas[3])
+console.log(comidasFavoritas[4])
+
+    EXERCÍCIO 3 */
+
+let listaDeTarefas =[]
+let tarefas = prompt("Não entendi o enunciado")
+
+tarefas = listaDeTarefas.push
+console.log(tarefas);
+
+
+    
+    
+
+    
+
+
+
